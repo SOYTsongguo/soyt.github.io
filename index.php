13ad0a88d11bc5995bfa842452fa96c6567a91fc
SOYTsongguo <159928980+SOYTsongguo@users.noreply.github.com>
2024-02-14 00:15:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+'--------------------------------------------------------
+'软件名称：MVSO
+'开发作者：GMeier
+*官方网站：http://www.gmeier.top/
+*
+'--------------------------------------------------------
+*/
+header('Content-Type:text/html;charset=utf-8');
+require __DIR__ . '/public/index.php';
