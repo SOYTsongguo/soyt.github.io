3b318c4a3e503e59674ead78b7c0d8929a1650f4
SOYTsongguo <159928980+SOYTsongguo@users.noreply.github.com>
2024-02-14 01:36:14 +0800

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+// 这是应用配置文件
+
+return
+[
+  'username' => 'mvso',
+  'password' => '123456',
+  'view' => 'etos',
+  'webtitle' => 'MVSO',
+  'keywords' => 'MVSO,搜索,视频搜索,搜索引擎,影视搜索,GMeier,ican,ican动漫,视频,电影',
+  'description' => '本网站为非盈利性站点， 本网站所有内容均来源于互联网相关站点自动搜索采集信息.',
+  'zy' => '',
+  'jx' => '接口一,http://video.eeeol.cn/jx.php?url=
+接口二,http://api.yscode.net/?url=
+接口三,http://www.2jiexi.cn/?url=
+接口四,http://jx.drgxj.com/?url=
+接口五,https://jx.xxdizhi.cc/?url=',
+  'copyright' => '本网站为非盈利性站点， 本网站所有内容均来源于互联网相关站点自动搜索采集信息.<br/><a target="_blank" style="color:#6c757d" href="http://www.yscode.net/">剧简影视</a>',
+  'link' => 'Ican,http://www.yscode.net/
+MVSO,http://www.yscode.net
+彼岸花商城,www.yscode.net',
+  'cnzz' => 'cnzz统计',
+  'author' => '源码站',
+  'url' => 'http://www.yscode.net/',
+];
